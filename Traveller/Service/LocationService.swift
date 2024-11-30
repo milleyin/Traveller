dc2a1e1e162618d4476980bdf182adb573195aec
Mille Yin <mille.yin@gmail.com>
2024-11-30 13:12:48 +0800

    完成构建定位接口

---
+++ Traveller/Service/LocationService.swift
@@ -0,0 +1,42 @@
+//
+//  LocationService.swift
+//  Traveller
+//
+//  Created by Mille Yin on 2024/11/30.
+//
+
+import SwiftUI
+import Combine
+import CoreLocation
+
+class LocationService: ObservableObject {
+    
+    static let shared = LocationService()
+    
+    ///当前位置(持续更新)
+    @Published var location: CLLocation = .init()
+    ///当前位置(单次更新)
+    var currentLocation: CLLocation? {
+        CoreLocationRepository.shared.currentLocation
+    }
+    
+    private var subscriptions = Set<AnyCancellable>()
+    
+    private init() {
+        updateLocation()
+    }
+    
+    private func updateLocation() {
+        CoreLocationRepository.shared.locationPublisher
+            .compactMap({ $0 })
+            .receive(on: RunLoop.main)
+            .sink { [weak self] location in
+                guard let self: LocationService else { return }
+                self.location = location
+            }.store(in: &self.subscriptions)
+    }
+    
+    deinit {
+        self.subscriptions.forEach { $0.cancel() }
+    }
+}
